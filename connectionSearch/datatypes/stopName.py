bb54a3689b1475226256361f112d7f1a2ebbbff8
Erik Řehulka <erik.rehulka@gmail.com>
2021-11-23 07:36:16 +0100

    Updated datatypes

--- connectionSearch/datatypes/stopName.py
+++ connectionSearch/datatypes/stopName.py
@@ -6,3 +6,9 @@ class StopName:
   
   def __str__(self):
     return self.name
+
+  def __add__(self, other):
+    return str(self) + other
+    
+  def __radd__(self, other):
+    return other + str(self)
--- connectionSearch/datatypes/time.py
+++ connectionSearch/datatypes/time.py
@@ -7,6 +7,9 @@ class Time:
   def __add__(self, other):
     return Time(self.seconds + other.seconds)
 
+  def __lt__(self, other):
+    return self.seconds < other.seconds
+
 @dataclass
 class TimeDiff(Time):
   pass
