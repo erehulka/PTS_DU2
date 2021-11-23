1a06a4de948e1b6df9854fd9a9c853085cbf1708
Erik Řehulka <erik.rehulka@gmail.com>
2021-11-23 07:36:50 +0100

    Added lines and stops classes

---
+++ connectionSearch/lines.py
@@ -0,0 +1,14 @@
+from typing import Dict, List
+from connectionSearch.line import Line
+from connectionSearch.datatypes.lineName import LineName
+from connectionSearch.datatypes.stopName import StopName
+from connectionSearch.datatypes.time import Time
+
+class Lines:
+
+  _lines: Dict[LineName, Line]
+
+  def updateReachable(self, lines: List[LineName], stop: StopName, time: Time) -> None:
+    for line in lines:
+      if line not in self._lines: continue
+      self._lines[line].updateReachable(time, stop)
---
+++ connectionSearch/stops.py
@@ -0,0 +1,35 @@
+from typing import Dict, List, Optional, Tuple
+from connectionSearch.stop import Stop
+from connectionSearch.datatypes.time import Time
+from connectionSearch.datatypes.stopName import StopName
+from connectionSearch.datatypes.lineName import LineName
+
+class Stops:
+
+  _stops: Dict[StopName, Stop]
+
+  def earliestReachableStopAfter(self, time: Time) -> Optional[ Tuple[StopName, Time] ]:
+    earliest: Optional[Time] = None
+    earliestStop: Optional[StopName] = None
+
+    for stopName in self._stops:
+      stop: Stop = self._stops[stopName]
+      reachableAt: Tuple[Optional[Time], Optional[LineName]] = stop.reachableAt
+      if reachableAt[0] is not None:
+        if earliest is None or reachableAt[0] < earliest:
+          earliest = reachableAt[0]
+          earliestStop = stop.name
+
+    if earliest is None or earliestStop is None: return None
+    
+    return (earliestStop, earliest)
+
+  def getLines(self, stop: StopName) -> List[LineName]:
+    if stop not in self._stops:
+      raise Exception("This stop name is not in dictionary of stops (name of stop: " + stop + ")")
+    return self._stops[stop].lines
+
+  def getReachableAt(self, stop: StopName) -> Tuple[Optional[Time], Optional[LineName]]:
+    if stop not in self._stops:
+      raise Exception("This stop name is not in dictionary of stops (name of stop: " + stop + ")")
+    return self._stops[stop].reachableAt
