03235fb7988ed715e83f7f3b30ff85775306f26d
Erik Řehulka <erik.rehulka@gmail.com>
2021-11-25 15:34:02 +0100

    Return ConnectionData object

--- connectionSearch/connectionSearch.py
+++ connectionSearch/connectionSearch.py
@@ -1,4 +1,5 @@
 from typing import List, Optional, Tuple
+from connectionSearch.datatypes.connectionData import ConnectionData
 from connectionSearch.datatypes.lineName import LineName
 from connectionSearch.stop import Stop
 from connectionSearch.stops import Stops
@@ -15,23 +16,22 @@ class ConnectionSearch:
     self._stops = stops
     self._lines = lines
 
-  def search(self, fr: StopName, to: StopName, time: Time):
+  def search(self, fr: StopName, to: StopName, time: Time) -> Optional[ConnectionData]:
     self._stops.setStartingStop(fr, time)
     linesFrom: List[LineName] = self._stops.getLines(fr)
     self._lines.updateReachable(linesFrom, fr, time)
 
     reachableAfter: Optional[ Tuple[StopName, Time] ] = self._stops.earliestReachableStopAfter(time)
-    if reachableAfter is None: 
-      print("Error") # TODO handling
-      return
+    if reachableAfter is None:
+      # TODO Logging
+      return None
     while reachableAfter is not None and reachableAfter[0] != to:
       newTime: Time = reachableAfter[1]
-      print(reachableAfter)
       linesFrom = self._stops.getLines(reachableAfter[0])
       self._lines.updateReachable(linesFrom, reachableAfter[0], newTime)
       reachableAfter = self._stops.earliestReachableStopAfter(newTime)
     
     if reachableAfter is not None:
-      print(reachableAfter[1])
+      return ConnectionData(fr, to, time, reachableAfter[1])
+    return None
     
-    print("Finished") # TODO Finish
